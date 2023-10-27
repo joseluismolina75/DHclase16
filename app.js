1299f3931eac4a2997dd3613ecd4da2c19d1da57
joseluismolina75 <joseluismolina75@gmail.com>
2023-10-27 13:22:18 -0300

    sistema de ruteo

--- app.js
+++ app.js
@@ -6,7 +6,19 @@ let app = express();
 
 app.listen(3000, () => console.log('Esto fue exitoso'));
 
-app.get('/', function(req, res) {
-    res.send('Bienvenidos al Sitio');
-}
-)
+
+app.get('/', function(req, res){
+    res.send('Bienvenidos al sitio');
+});
+
+app.get('/contacto', function(req, res){
+    res.send('Dejanos tu contacto');
+});
+
+app.get('/un-array', function(req, res){
+    res.send([1,2,3,4]);
+});
+
+app.get('/un-objeto', function(req, res){
+    res.send({name: "Dario"});
+});
